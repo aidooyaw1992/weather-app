39b1239610723c151e8cd9cefe3321242b3c17e1
Daniel Aidoo <aidooyaw1992@gmail.com>
2025-09-17 18:44:29 +0000

    feat: added theme store

---
+++ store/useAppThemeStore.ts
@@ -0,0 +1,23 @@
+
+import { ThemeType } from '@/types'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+interface ThemeState {
+  theme: ThemeType
+  setTheme: (theme: ThemeType) => void
+}
+
+export const useThemeStore = create<ThemeState>()(
+  persist(
+    (set) => ({
+      theme: 'system',
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: 'theme-store',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+)
